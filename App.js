73ba15ca42458653a82d1fc007dfc5fa8baad35c
MajuZamith <mjulia_zamith@outlook.com>
2022-09-15 21:03:23 -0300

    Aplicativo Turimo

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import turismo from './assets/turismo.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up teste App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={turismo} defaultSource={turismo} style={styles.foto}></Image>
+      <Text style={styles.FirstText}> Zamith turismo</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aqua',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  foto:{
+    width:320,
+    height:300,
+  },
+  FirstText:{
+    fontSize:40,
+    lineHeight:47,
+    fontWeight:'bold',
+    color:'black',
+    padding:'10',
+    textAlign:'center'
   },
 });
